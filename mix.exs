cfd3e36d147f5b3baf64c0300bd274009a78de03
James Moore <james@knowthen.com>
2020-01-31 16:40:16 -0800

    add pow_assent

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule TaskApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pow, "~> 1.0"}
+      {:pow, "~> 1.0"},
+      {:pow_assent, "~> 0.4"}
     ]
   end
 
