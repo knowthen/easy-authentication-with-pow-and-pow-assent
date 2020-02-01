c617c447bad1e137264643f29a5690c1173728a3
James Moore <james@knowthen.com>
2020-01-31 16:07:34 -0800

    rudimentary/route based authorization

--- lib/task_app_web/router.ex
+++ lib/task_app_web/router.ex
@@ -14,6 +14,11 @@ defmodule TaskAppWeb.Router do
     plug :accepts, ["json"]
   end
   
+  pipeline :protected do
+    plug Pow.Plug.RequireAuthenticated,
+      error_handler: Pow.Phoenix.PlugErrorHandler
+  end
+  
   scope "/" do
     pipe_through :browser
 
@@ -27,7 +32,7 @@ defmodule TaskAppWeb.Router do
   end
   
   scope "/", TaskAppWeb do
-    pipe_through :browser
+    pipe_through [:browser, :protected]
     
     resources "/tasks", TaskController  
   end
